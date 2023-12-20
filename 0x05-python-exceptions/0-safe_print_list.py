7e61d0b1440b6d3510bc09e27946b263f10a5871
eunicePops <enablersolutions@gmail.com>
2023-12-19 17:26:37 -0800

    safe

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    e = 0
+    for n in range(x):
+        try:
+            print(my_list[n], end="")
+            e += 1
+        except IndexError:
+            break
+    print()
+    return e
