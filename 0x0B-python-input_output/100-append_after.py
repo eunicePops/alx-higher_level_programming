ef08d4099d78386e450fc3946b8c8bf7592afb5c
eunicePops <enablersolutions@gmail.com>
2024-01-10 14:32:11 -0800

    fix

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -7,10 +7,11 @@ def append_after(filename="", search_string="", new_string=""):
     """Inserts text after each line containing a given string in a file
     """
     text = ""
-    with open(filename) as r:
-        for line in r:
-            text += line
+    with open(filename, 'r') as file:
+        lines = file.realines()
+            
+    with open(filename, "w") as file:
+        for line in lines:
+            file.write(line)
             if search_string in line:
-                text += new_string
-    with open(filename, "w") as w:
-        w.write(text)
+                file.write(new_string + '\n')
